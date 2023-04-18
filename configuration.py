d7f4467b25d56a60735319dbe699c87f5cece7cb
Vasyukov Nikolay <vasyukovn@gmail.com>
2023-04-18 13:31:33 +0300

    changes url

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://44e36946-a023-45b0-9a9e-0ec56d4619ce.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://8ead1711-b42a-4ffa-b660-ac9882468b93.serverhub.praktikum-services.ru"
 CREATE_PRODUCT_KITS = "/api/v1/kits/"
 CREATE_USER_PATH = "/api/v1/users/"
